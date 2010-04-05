d5968c353eabf38ef4dabe84da35a742e756d7aa
choonkeat <choonkeat@gmail.com>
2010-04-05 17:02:25 +0800

    bugfix to save binary_column even when parent is not dirty; tracking original_filename if possible

--- lib/binary_column_table.rb
+++ lib/binary_column_table.rb
@@ -20,6 +20,7 @@ module ActiveRecord
                 def #{colname}=(val)
                   if not val.blank?
                     self.#{colname}_binary_column ||= #{BinaryColumnTable::CLASS_NAME}.new(:original_table => self, :name => "#{colname}")
+                    self.#{colname}_binary_column.original_table_id = nil # ensure this object is saved, even when parent is not dirty
                     self.#{colname}_binary_column.content = val.respond_to?(:read) ? val.read : val
                     if self.#{colname}_binary_column.respond_to?(:content_type)
                       # optional content_type extraction
@@ -29,7 +30,8 @@ module ActiveRecord
                         else
                           Tempfile.open("binary_column_table") {|f| filepath = f.path; f.write self.#{colname}_binary_column.content }
                         end
-                        self.#{colname}_binary_column.content_type = IO.popen("file --mime \#{filepath.inspect}") {|io| io.gets.sub(/^.+?: /, '').strip }
+                        self.#{colname}_binary_column.content_type = IO.popen("file --mime \#{filepath.inspect}") {|io| io.gets.split(/:\s*/).last.strip }
+                        self.#{colname}_binary_column.original_filename = val.original_filename if self.#{colname}_binary_column.respond_to?(:original_filename=) && val.respond_to?(:original_filename)
                       rescue Exception, IOError
                       end
                     end
